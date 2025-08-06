2166cbb82e05676de97d0e66ca4f73d55a206e1d
Kunal <KunalK180305@gmail.com>
2025-08-06 20:54:16 +0530

    added email verification

--- task2backend/routes/authRoutes.js
+++ task2backend/routes/authRoutes.js
@@ -1,54 +1,54 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import validator from "validator";
 
 const router = express.Router();
-import { User } from "../models/user.models.js";
+import {User} from "../models/user.models.js";
 
 const generateToken = (user) => {
-    return jwt.sign({ id: user._id, email: user.email, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    return jwt.sign({id: user._id, email: user.email, role: user.role}, process.env.JWT_SECRET, {expiresIn: "1h"});
 }
 
 router.post("/signup", async (req, res) => {
-    const { username, email, password } = req.body;
+    const {username, email, password} = req.body;
 
-    if (!username || !email || !password) {
-        res.status(401).json({ message: "All credentials are required!" });
-    }
 
-    const existingUser = await User.findOne({ email });
+    if (!username || !email || !password) return res.status(401).json({message: "All credentials are required!"});
+    if (!validator.isEmail(email)) return res.status(400).json({message: "Invalid email format"});
+
+    const existingUser = await User.findOne({email});
     if (existingUser) {
-        return res.status(400).json({ message: "User already exists!" });
+        return res.status(400).json({message: "User already exists!"});
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = new User({ username, email, password: hashedPassword });
+    const user = new User({username, email, password: hashedPassword});
     await user.save();
 
-    res.status(201).json({ message: "Sign up successfull", user });
+    res.status(201).json({message: "Sign up successfull", user});
 });
 
 router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
+    const {email, password} = req.body;
 
-    if (!email || !password) {
-        res.status(401).json({ message: "All credentials are required!" });
-    }
+    if (!email || !password) return res.status(401).json({message: "All credentials are required!"});
+    if (!validator.isEmail(email)) return res.status(400).json({message: "Invalid email format"});
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({email});
     if (!user) {
-        res.status(400).json({ message: "Invalid credentials!" });
+        res.status(400).json({message: "Invalid credentials!"});
     }
 
     const isUser = await bcrypt.compare(password, user.password);
     if (!isUser) {
-        res.status(400).json({ message: "Incorrect password!" });
+        res.status(400).json({message: "Incorrect password!"});
     }
 
     const token = generateToken(user);
 
-    res.status(201).json({ message: "Logged in successfully", user, token });
+    res.status(201).json({message: "Logged in successfully", user, token});
 });
 
 export default router;
