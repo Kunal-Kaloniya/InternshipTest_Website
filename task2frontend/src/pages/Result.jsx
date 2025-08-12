265369aa43f1fa839f3e7586f614db520fc019dd
Kunal <KunalK180305@gmail.com>
2025-08-12 22:37:23 +0530

    Minor layout changes

--- task2frontend/src/pages/Result.jsx
+++ task2frontend/src/pages/Result.jsx
@@ -22,8 +22,8 @@ function Result() {
     }, [result])
 
     return (
-        <div className="font-mono min-h-screen pt-[10vh] bg-gray-200 dark:bg-gray-700 text-black dark:text-white transition-all">
-            <div id="main" className="max-w-[90vw] mt-5 py-10 mx-auto rounded-xl text-black bg-white dark:bg-gray-800 dark:text-white">
+        <div className="font-mono py-5 bg-gray-200 dark:bg-gray-700 text-black dark:text-white transition-all">
+            <section className="max-w-[80vw] py-10 mx-auto rounded-xl text-black bg-white dark:bg-gray-800 dark:text-white">
                 <div className="text-center pb-10 text-xl transition-all">
                     <h1 className="text-3xl font-bold mb-5">Your score is {result} out of {questions.length}.</h1>
                     <p>You have attempted {Object.keys(selectedAnswers).length} out of {questions.length} questions</p>
@@ -36,17 +36,17 @@ function Result() {
                         )
                     }
                 </div>
-            </div>
+            </section>
 
-            <div id="footer" className="max-w-[90vw] h-auto bg-gray-100 mt-5 mx-auto rounded-xl p-3 text-black bg-white dark:bg-gray-800 dark:text-white transition-all">
-                <h1 className="text-xl font-bold text-center mb-3">Explanation: </h1>
+            <section className="max-w-[90vw] h-auto mt-5 mx-auto rounded-xl p-3 text-black bg-white dark:bg-gray-800 dark:text-white transition-all">
+                <h2 className="text-xl font-bold text-center mb-3">Explanation: </h2>
                 <div className="grid grid-cols-2 gap-2">
                     {
                         questions.length !== 0 && (
                             questions.map((q, index) => {
                                 const isCorrect = questions[index].correctAnswer === selectedAnswers[q._id];
                                 return (
-                                    <div className={`${isCorrect ? "bg-green-200" : "bg-red-200"} rounded-xl border-2 px-4 py-2 text-black dark:border-0`} key={index}>
+                                    <div className={`${isCorrect ? "bg-green-100 border-green-400" : "bg-red-100 border-red-400"} border rounded-xl px-4 py-2 text-black`} key={index}>
                                         <h1><span className="font-black">Q{index + 1}:</span> {q.question}</h1>
                                         <p><span className="font-black">Correct Answer:</span> {q.correctAnswer}</p>
                                         <p><span className="font-black">Your Answer:</span> {selectedAnswers[q._id] ? selectedAnswers[q._id] : "N/A"}</p>
@@ -57,7 +57,7 @@ function Result() {
                         )
                     }
                 </div>
-            </div>
+            </section>
         </div>
     );
 }
